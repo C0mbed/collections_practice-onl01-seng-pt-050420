05989e737e04a0d6ef1d67c1561651919d5d2e3c
Matt Farncombe <matt@farncombe.co>
2020-05-10 11:34:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(num)
+  num.sort
+end
+
+def sort_array_desc(num)
+  num.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count(words)
+  words.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements(collection)
+  collection[1], collection[2] = collection[0], collection[2], collection[1]
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker(nums)
+  nums.each do |num|
+    num[2] = "$"
+  end
+end
+
+def find_a(words)
+  
+end
 
